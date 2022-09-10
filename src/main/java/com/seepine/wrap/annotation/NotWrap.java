4f5001c290d603e0e1b990d81bf4ee9440e1e6b9
seepine <seepine@outlook.com>
2022-09-10 15:37:14 +0800

    fix lf

--- src/main/java/com/seepine/wrap/annotation/NotWrap.java
+++ src/main/java/com/seepine/wrap/annotation/NotWrap.java
@@ -5,7 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** @author seepine */
+/**
+ * @author seepine
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotWrap {}
