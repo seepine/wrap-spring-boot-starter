2bf866499857550cb9a8874d8c9a2ba9009d5121
seepine <93666053+seepine@users.noreply.github.com>
2023-06-15 07:02:14 +0000

    fix(exception): fix response content type utf8

--- src/main/java/com/seepine/wrap/WrapExceptionHandler.java
+++ src/main/java/com/seepine/wrap/WrapExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
@@ -21,8 +20,13 @@ import java.util.Objects;
  */
 @RestControllerAdvice
 public class WrapExceptionHandler implements Ordered {
+  private final static String APPLICATION_JSON = "application/json;charset=UTF-8";
 
-  @Resource private WrapProperties wrapProperties;
+  private final WrapProperties wrapProperties;
+
+  public WrapExceptionHandler(WrapProperties wrapProperties) {
+    this.wrapProperties = wrapProperties;
+  }
 
   @Override
   public int getOrder() {
@@ -32,54 +36,58 @@ public class WrapExceptionHandler implements Ordered {
   /**
    * 拦截流程异常 WrapException
    *
-   * @param e WrapException
+   * @param e        WrapException
    * @param response res
    * @return R
    */
   @ExceptionHandler(WrapException.class)
-  public R<Object> workflowException(final WrapException e, HttpServletResponse response) {
+  public R<?> workflowException(final WrapException e, HttpServletResponse response) {
     response.setStatus(e.getStatus() == null ? wrapProperties.getStatus() : e.getStatus());
+    response.setContentType(APPLICATION_JSON);
     return R.fail(e.getData(), e.getMessage());
   }
 
   /**
    * 拦截绑定参数校验提示语
    *
-   * @param e e
+   * @param e        e
    * @param response res
    * @return R
    */
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public R<Object> methodArgumentNotValidException(
+  public R<?> methodArgumentNotValidException(
       MethodArgumentNotValidException e, HttpServletResponse response) {
     response.setStatus(wrapProperties.getStatus());
+    response.setContentType(APPLICATION_JSON);
     return R.fail(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
   }
 
   /**
    * 拦截绑定参数校验提示语
    *
-   * @param e e
+   * @param e        e
    * @param response res
    * @return R
    */
   @ExceptionHandler(BindException.class)
-  public R<Object> handleBindException(BindException e, HttpServletResponse response) {
+  public R<?> handleBindException(BindException e, HttpServletResponse response) {
     response.setStatus(wrapProperties.getStatus());
+    response.setContentType(APPLICATION_JSON);
     return R.fail(e.getAllErrors().get(0).getDefaultMessage());
   }
 
   /**
    * 拦截接口请求参数格式错误友好提示
    *
-   * @param e e
+   * @param e        e
    * @param response res
    * @return R
    */
   @ExceptionHandler(HttpMessageNotReadableException.class)
-  public R<Object> httpMessageNotReadableException(
+  public R<?> httpMessageNotReadableException(
       HttpMessageNotReadableException e, HttpServletResponse response) {
     response.setStatus(wrapProperties.getStatus());
+    response.setContentType(APPLICATION_JSON);
     if (e.getMessage() != null
         && e.getMessage().contains("Cannot deserialize value of type `long` from String")) {
       return R.fail("接口参数格式错误，请输入纯数字");
@@ -90,14 +98,15 @@ public class WrapExceptionHandler implements Ordered {
   /**
    * 拦截接口请求参数格式错误友好提示
    *
-   * @param e e
+   * @param e        e
    * @param response res
    * @return R
    */
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-  public R<Object> methodArgumentTypeMismatchException(
+  public R<?> methodArgumentTypeMismatchException(
       MethodArgumentTypeMismatchException e, HttpServletResponse response) {
     response.setStatus(wrapProperties.getStatus());
+    response.setContentType(APPLICATION_JSON);
     if (e.getMessage() != null
         && e.getMessage()
             .contains(
@@ -110,14 +119,15 @@ public class WrapExceptionHandler implements Ordered {
   /**
    * 拦截接口请求方法错误友好提示
    *
-   * @param e e
+   * @param e        e
    * @param response res
    * @return R
    */
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-  public R<Object> httpRequestMethodNotSupportedException(
+  public R<?> httpRequestMethodNotSupportedException(
       HttpRequestMethodNotSupportedException e, HttpServletResponse response) {
     response.setStatus(wrapProperties.getStatus());
+    response.setContentType(APPLICATION_JSON);
     return R.fail(e.getMessage());
   }
 }
